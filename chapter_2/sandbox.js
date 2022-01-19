7a47dab5048e0fc3cc69637f94045d47eaee56e2
EBOBO MAXWELL OGHENEMAGA <oghenemaga.ebobo@unn.edu.ng>
2022-01-19 10:59:54 +0100

    using strict comparison

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -38,3 +38,39 @@ num4+=10;
 let num5 = num1 - 10;
 // num5-=10;
 console.log(num5);
+// template strings
+const title = "Code with Javascript";
+const author = "Ebobo";
+const likes = "10";
+const result = `The book titled ${title} is authoured by ${author} and has ${likes} likes`;
+// document.write(result);
+//creating html templates
+let html = `
+<p style="color: red">The book titled</p> 
+<h2>${title}</h2>
+is authoured by ${author} and has ${likes} likes`
+document.write(html);
+//Arrays 
+let ninja = ['max','sixtus','chibike','terry','john'];
+console.log(ninja[3]);
+//overiding a position
+ninja[3] = "Dan";
+console.log(ninja[3]);
+//arrays with some properties
+console.log(ninja.join('-')); //using the join properties.
+console.log(ninja.length); //using the length properties.
+// let result1 = ninja.pop();//to remove the last item.
+// ninja.push('Matthew');// adding an item to the array
+// let result1 = ninja.shift('Dan');
+// let result1 = ninja.indexOf('sixtus');  to show the index number of sixtus
+// let result1 = ninja.concat('max', 'Matthew'); to add to string together.
+let result1 = ninja.includes('sixtus');   //assertian if a value is inluded
+console.log(result1);
+// console.log(resultB);
+
+// Boolean
+let a = 1;
+let b = 2;
+console.log(b==='2');  //using strict comparison
+console.log(b !=='2');  //using strict comparison
+console.log(b !== 2);   //using strict comparison
