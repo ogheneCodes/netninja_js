852fba279d40b76742e51c23a3694553f19fd9ac
EBOBO MAXWELL OGHENEMAGA <oghenemaga.ebobo@unn.edu.ng>
2022-01-19 14:03:49 +0100

    using the for loop in control flow

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,9 @@
+//for loops
+for(let i = 0; i < 5; i++){
+    document.write('i in for loop is less than:', i + "<br><br>")
+}
+const name = ['max', 'sixtus', 'chibike', 'Terry'];
+ for(e=0; e <name.length; e++){
+    let html = `<div>${name[e]}</div>`
+    document.write(html);
+ }
