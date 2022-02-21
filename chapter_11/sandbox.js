a2512ccce843babc4c1b2af7e59c1ebda11367aa
EBOBO MAXWELL OGHENEMAGA <oghenemaga.ebobo@unn.edu.ng>
2022-02-21 20:39:34 +0100

    [digital watch]

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -13,8 +13,26 @@ const hours = Math.round(mins / 60);
 const days = Math.round(hours / 24);
 
 console.log(`mins: ${mins}, hours:${hours}, days:${days}`);
-console.log(`the blog was written ${days} ago`);
+console.log(`the blog was written ${days} days ago`);
 
 //converting timestamps into objects
 const timestamps = 1645462829804;
 console.log(new Date(timestamps));
+
+//building a Digital Clock
+const clock = document.querySelector('.clock');
+
+const tick = () =>{
+    const now = new Date();
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getMinutes();
+
+    const html = `
+        <span>${h}</span>:
+        <span>${m}</span>:
+        <span>${s}</span>
+    `;
+    clock.innerHTML= html;
+};
+setInterval(tick, 1000);
