f0dfb4d044fa28224a80deb63824588b100e33c6
EBOBO MAXWELL OGHENEMAGA <oghenemaga.ebobo@unn.edu.ng>
2022-03-01 16:02:46 +0100

    [using promise in asyc javascript]

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,30 +1,40 @@
-const getTodos = (resource, callback)=>{
+const getTodos = (resource, callback) => {
+
+
+  return new Promise((resolve, reject) => {    //using a promise function here
     const request = new XMLHttpRequest();
-// console.log(request);
+    // console.log(request);
 
-
-//to track the progress of an event in async JavaScript
-request.addEventListener('readystatechange', ()=>{
-    if(request.readyState === 4 && request.status === 200){
-        const data = JSON.parse(request.responseText)
-        callback(undefined, data);
-    }else if(request.readyState === 4){
-        callback('could not fetch data', undefined);
-    }
-});
-
-request.open('GET', resource);
-
-request.send();
-
-};
-
-getTodos('todos/sixtus.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/max.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/terry.json', (err, data) => {
-            console.log(data);
-        });
+    //to track the progress of an event in async JavaScript
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data); //rather than callback, we use resolve
+      } else if (request.readyState === 4) {
+        reject('error getting resource') //rather than callback we use reject
+      }
     });
+
+    request.open("GET", resource);
+    request.send();
+  });
+};
+getTodos('todos/max.json').then(data => {
+    console.log('promise resolved:', data);
+}).catch((err)=>{
+    console.log('promise rejected:',err);
 });
+
+
+
+
+
+// getTodos('todos/sixtus.json', (err, data) => {
+//     console.log(data);
+//     getTodos('todos/max.json', (err, data) => {
+//         console.log(data);
+//         getTodos('todos/terry.json', (err, data) => {
+//             console.log(data);
+//         });
+//     });
+// });
